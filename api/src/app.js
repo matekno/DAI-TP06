a54ad3bb05a02376073919e43962c2f8d0dfd76a
matekno <mati.rapoport@gmail.com>
2022-07-07 08:59:15 -0300

    bases con firebase

--- api/src/app.js
+++ api/src/app.js
@@ -3,11 +3,26 @@ import logger from './utils/logger.js'
 import CAVRouter from "./controllers/PersonajesController.js";
 import cors from 'cors'
 import bp from 'body-parser';
+import * as Config from "./config/firebase_config.js"
+
+import { initializeApp } from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+import * as url from 'url';
+import path from "path";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+
+
+initializeApp(Config.firebaseConfig)
 
 const app = express();
 const port = 3000;
 
-const logOnFinish = (req, res, next) =>{
+const logOnFinish = (req, res, next) => {
     // console.log('antes: ' + new Date().toISOString());
     //console.log(req.method);
     next();
@@ -22,13 +37,18 @@ const logOnFinish = (req, res, next) =>{
 
 app.use(cors());
 app.use(bp.json());
-app.use(bp.urlencoded({ extended: true }))
+app.use(bp.urlencoded({ extended: true }));
 app.use(logOnFinish);
 
 app.use("/api/characters", CAVRouter);
 
+app.use(express.static("public"))
 
-app.listen(port, () =>{
+app.get("/", async (req, res) => {
+    res.sendFile(path.join(__dirname, './public', 'index.html'));
+})
+
+app.listen(port, () => {
     logger.log({
         level: 'info',
         message: `Started in port ${port}`
